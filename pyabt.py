546bfb77867543e0fd645fe621873e2a6bc907ff
ZhaoymOvO <zhaoym233@hotmail.com>
2024-10-19 00:27:16 +0800

    fix sox discover error

--- pyabt.py
+++ pyabt.py
@@ -4,7 +4,7 @@ import os
 import re
 import sys
 
-_helpmsg = '''
+_helpmsg = """
 PyABT, a Python audio batch transcoder depends on SoX
 Get SoX: https://sourceforge.net/projects/sox/
 
@@ -35,18 +35,27 @@ Usage:
 
     main.py [-h|--help]
         Print this help massage
-'''
+"""
 
-has_sox = re.findall(r'[sS][oO][xX]', os.getenv('PATH'))
+has_sox = False
+_path = os.getenv("PATH").split(":")
+for i in _path:
+    try:
+        if "sox" in os.listdir(i):
+            has_sox = True
+            break
+    except FileNotFoundError:
+        continue
+
 argv = sys.argv[1:]
 
-if '-h' in argv or '--help' in argv:
+if "-h" in argv or "--help" in argv:
     print(_helpmsg)
     exit()
 
 if not has_sox:
-    print('This script needs <SoX> to run')
-    print('For get <SoX>, visit: https://sourceforge.net/projects/sox/')
+    print("This script needs <SoX> to run")
+    print("For get <SoX>, visit: https://sourceforge.net/projects/sox/")
     exit(1)
 
 if len(argv) == 4:
@@ -55,24 +64,30 @@ if len(argv) == 4:
     target_path = argv[2]
     target_format = argv[3]
 elif len(argv) != 4 and argv:
-    print('Incorrect input parameters')
-    print('Use <main.py -h> to see help')
+    print("Incorrect input parameters")
+    print("Use <main.py -h> to see help")
     exit(1)
 else:
-    source_path = input('Source path: ')
-    source_format = input('Source format (extension name): ')
-    target_path = input('Target/Output path: ')
-    target_format = input('Target format(extension name): ')
+    source_path = input("Source path: ")
+    source_format = input("Source format (extension name): ")
+    target_path = input("Target/Output path: ")
+    target_format = input("Target format(extension name): ")
 
 source_file_list = []
 for i in os.listdir(source_path):
-    if i.endswith(f'.{source_format}'):
+    if i.endswith(f".{source_format}"):
         source_file_list.append(i)
 
-target_file_list = [f"{re.findall(r'(.+)\..+?', i)[0]}.{target_format}" for i in source_file_list]
+target_file_list = [
+    f"{re.findall(r'(.+)\..+?', i)[0]}.{target_format}" for i in source_file_list
+]
 
 for i in range(len(source_file_list)):
-    if os.name == 'nt':
-        os.system(f"sox '{source_path}\\{source_file_list[i]}' '{target_path}\\{target_file_list[i]}'")
+    if os.name == "nt":
+        os.system(
+            f"sox '{source_path}\\{source_file_list[i]}' '{target_path}\\{target_file_list[i]}'"
+        )
     else:
-        os.system(f"sox '{source_path}/{source_file_list[i]}' '{target_path}/{target_file_list[i]}'")
+        os.system(
+            f"sox '{source_path}/{source_file_list[i]}' '{target_path}/{target_file_list[i]}'"
+        )
